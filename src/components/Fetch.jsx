b99b6c78fb673236d2b06be88aa1e18541626710
Ian Cahn <iancahn@live.com>
2022-08-22 20:52:59 +0200

    Fetched API data

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,14 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Fetch() {
-    useEffect(() => {
+    const [posts, setPosts] = useState([]);
+    const [randomUserDataJSON, setRandomUserDataJSON] = useState('');
 
+    useEffect(() => {
+        fetch('https://randomuser.me/api/')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setPosts(data);
+                return setRandomUserDataJSON(JSON.stringify(data));
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
     }, [])
-    // https://randomuser.me/api/
 
     return (
         <div>
-            <h1>Hello</h1>
+            <h2>{randomUserDataJSON}</h2>
         </div>
     )
 }
